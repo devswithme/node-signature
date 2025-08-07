65cf8f3cf3b959a1302a5dda5b640fe38169c886
devswithme <developswithme@gmail.com>
2025-08-07 16:39:26 +0800

    add key

--- src/app/api/signature/route.ts
+++ src/app/api/signature/route.ts
@@ -3,16 +3,22 @@ import crypto from "crypto";
 
 export async function POST(req: NextRequest) {
   try {
-    const { merchant_ref, amount } = await req.json();
-    const privateKey = process.env.PRIVATE_KEY!;
-    const merchant_code = process.env.MERCHANT_CODE!;
+    if (
+      req.headers.get("Authorization")?.split(" ")[1] == process.env.API_KEY
+    ) {
+      const { merchant_ref, amount } = await req.json();
+      const privateKey = process.env.PRIVATE_KEY!;
+      const merchant_code = process.env.MERCHANT_CODE!;
 
-    const hmac = crypto.createHmac("sha256", privateKey);
-    hmac.update(`${merchant_code}${merchant_ref}${amount}`);
+      const hmac = crypto.createHmac("sha256", privateKey);
+      hmac.update(`${merchant_code}${merchant_ref}${amount}`);
 
-    const signature = hmac.digest("hex");
+      const signature = hmac.digest("hex");
 
-    return NextResponse.json({ success: true, data: signature });
+      return NextResponse.json({ success: true, data: signature });
+    } else {
+      return NextResponse.json({ success: false, data: "Unautorized" });
+    }
   } catch (err) {
     return NextResponse.json({ success: false, data: err });
   }
