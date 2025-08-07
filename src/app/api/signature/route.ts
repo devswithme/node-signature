19594f3e3f71c592935e689b99e6ca692626ec62
devswithme <developswithme@gmail.com>
2025-08-07 19:03:16 +0800

    add verify

--- src/app/api/signature/route.ts
+++ src/app/api/signature/route.ts
@@ -10,14 +10,39 @@ export async function POST(req: NextRequest) {
       const privateKey = process.env.PRIVATE_KEY!;
       const merchant_code = process.env.MERCHANT_CODE!;
 
-      const hmac = crypto.createHmac("sha256", privateKey);
-      hmac.update(`${merchant_code}${merchant_ref}${amount}`);
-
-      const signature = hmac.digest("hex");
+      const signature = crypto
+        .createHmac("sha256", privateKey)
+        .update(`${merchant_code}${merchant_ref}${amount}`)
+        .digest("hex");
 
       return NextResponse.json({ success: true, data: signature });
     } else {
-      return NextResponse.json({ success: false, data: "Unautorized" });
+      return NextResponse.json({ success: false, data: "Unauthorized" });
+    }
+  } catch (err) {
+    return NextResponse.json({ success: false, data: err });
+  }
+}
+
+export async function GET(req: NextRequest) {
+  try {
+    if (
+      req.headers.get("Authorization")?.split(" ")[1] == process.env.API_KEY
+    ) {
+      const { body, signature } = await req.json();
+      const privateKey = process.env.PRIVATE_KEY!;
+
+      const hmac = crypto
+        .createHmac("sha256", privateKey)
+        .update(JSON.stringify(body))
+        .digest("hex");
+
+      return NextResponse.json({
+        success: true,
+        data: Boolean(hmac == signature),
+      });
+    } else {
+      return NextResponse.json({ success: false, data: "Unauthorized" });
     }
   } catch (err) {
     return NextResponse.json({ success: false, data: err });
